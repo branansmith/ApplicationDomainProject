3bb64783704080dc5bc8482a2d633892463a215f
branansmith <137443505+branansmith@users.noreply.github.com>
2023-11-02 15:40:12 -0400

    Update user.js

--- user.js
+++ user.js
@@ -96,20 +96,10 @@ function addToTable(username, name, email, role, address, dob) {
 
 const createNewUserButton = document.getElementById('create-new-user-button');
 createNewUserButton.addEventListener("click", (e) => {
-  
+  alert("test");
 })
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
 
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-openModalBtn.addEventListener("click", openModal);
 
 async function isManager(user) {
   const museums = query(collection(db, 'users'), where('email', '==', user.email), where('role', '==', 'manager'));
