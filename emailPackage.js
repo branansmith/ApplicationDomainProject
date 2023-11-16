349eb1face65b671ade54d63a2116ee2764cd600
bulloa2432 <66567671+bulloa2432@users.noreply.github.com>
2023-11-16 15:51:09 -0500

    Add files via upload

---
+++ emailPackage.js
@@ -0,0 +1,23 @@
+{
+
+"name": "sendgrid",
+"version": "1.0.0",
+"description": "",
+"main": "index.js",
+
+"scripts": {
+    "test" "echo \"Error: no test specified\" && exit 1"
+},
+"keywords":[],
+"author": "",
+"license": "ISC",
+"dependencies": {
+    "@sendgrid/mail": "^7.4.0",
+    "dotenv": "^8.2.0"
+
+}
+}
+
+const api_key= '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';
+
+
